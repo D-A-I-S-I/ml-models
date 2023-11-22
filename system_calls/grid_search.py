2f45ee0fca8d352ad7348cf59ee93854b6336e52
Johannes Liljedahl <61147982+johanneslil@users.noreply.github.com>
2023-11-22 23:49:17 +0100

    perf: improved grid search (#1)
    * perf: Add Early stopping
    
    * removed prints
    
    * monitoring adjustments

--- system_calls/grid_search.py
+++ system_calls/grid_search.py
@@ -4,66 +4,125 @@ from itertools import product
 import multiprocessing
 
 # Hyperparameters
-hidden_dims = [16, 32, 64]
-embedding_dims = [10, 20, 30]
-encoding_dims = [4, 8, 16]
-batch_sizes = [256, 512, 1024]
+hidden_dims = [8, 16]
+embedding_dims = [10]
+encoding_dims = [4, 8]
+batch_sizes = [256]
 learning_rates = [0.001]
-sequence_lengths = [7, 14]
-num_epochs = 6
+sequence_lengths = [5, 10]
+patience = 4
+num_epochs = 50
+val_split = 0.3
 
-# NOTE: Make sure to change paths accordingly!
+# NOTE: Make sure to change paths to location of dataset!
 folder_path = '../../ADFA-LD-Dataset/ADFA-LD/Training_Data_Master/'
 attack_data_master_path = '../../ADFA-LD-Dataset/ADFA-LD/Attack_Data_Master/'
 
 # Create all combinations of hyperparameters
-all_combinations = list(product(hidden_dims, embedding_dims, encoding_dims, batch_sizes, learning_rates, sequence_lengths, [num_epochs]))
 
 def multiprocessing_wrapper(combination):
     """
     Wrapper function for multiprocessing. Returns the train and attack loss for the given hyperparameters.
     """
-    hidden_dim, embedding_dim, encoding_dim, batch_size, lr, sequence_length, num_epochs = combination
-    train_dataset, train_loader, num_unique_syscalls = load_data(folder_path, sequence_length, batch_size) # TODO: move all load_data calls to helper file
+    # print("Process ID: {}, Hyperparameters: {}".format(multiprocessing.current_process().pid, combination))
+    hidden_dim, embedding_dim, encoding_dim, batch_size, lr, sequence_length, num_epochs, patience, val_split = combination
+    train_dataset, train_loader, val_loader, num_unique_syscalls = load_data(folder_path, sequence_length, batch_size, val_split)
 
-    return train_and_evaluate(train_loader, sequence_length, num_epochs, train_dataset,
+    return train_and_evaluate(train_loader, val_loader, sequence_length, num_epochs, train_dataset,
                                attack_data_master_path, num_unique_syscalls, hidden_dim,
-                                 embedding_dim, encoding_dim, batch_size, lr)
+                                 embedding_dim, encoding_dim, batch_size, lr, patience)
+
+def run_grid_search(pool, all_combinations):
+    """
+    Runs the grid search using multiprocessing. Returns the results.
+    """
+    total = len(all_combinations)
+    done = 0
+    results = []
+
+    for result in pool.imap_unordered(multiprocessing_wrapper, all_combinations):
+        done += 1
+        print(f"Progress: {done}/{total} combinations done ({(done/total)*100:.2f}%)")
+        results.append(result)
+
+    return results
+
 
 if __name__ == "__main__":
+    cores = multiprocessing.cpu_count()
+    print('CPU count:', cores)
+    
+    all_combinations = list(product(hidden_dims, embedding_dims, encoding_dims, batch_sizes, learning_rates, sequence_lengths, [num_epochs], [patience], [val_split]))
+    print(f"Total number of combinations: {len(all_combinations)}")
+    num_processes = len(all_combinations) if len(all_combinations) < cores else cores
 
-    print('CPU count:', multiprocessing.cpu_count())
-
-    # Start the grid search using multiprocessing
-    with Pool(processes=multiprocessing.cpu_count()) as pool:
-        results = pool.map(multiprocessing_wrapper, all_combinations)
-
-    # Close the pool
-    pool.close()
-    pool.join()
+    # Start the grid search WITH multiprocessing
+    with Pool(processes=(num_processes)) as pool: # NOTE: Leave 2 cores for other processes, change if desired
+        results = run_grid_search(pool, all_combinations)
+    
+    # Start the grid search WITHOUT multiprocessing
+    # results = map(multiprocessing_wrapper, all_combinations)
 
     # Find the best model
     best_model_info = {
         'train_loss': float('inf'),
         'attack_loss': float('-inf'),
+        'val_loss': float('inf'),
+        'atk_val_ratio': float('-inf'),
         'hidden_dim': None,
         'embedding_dim': None,
         'encoding_dim': None,
         'batch_size': None,
-        'learning_rate': None
+        'learning_rate': None,
+        'sequence_length': None
+
     }
 
+    # Initialize a list to store the results
+    top_models = []
+
     for result in results:
-        train_loss, attack_loss, hidden_dim, embedding_dim, encoding_dim, batch_size, lr = result
-        if train_loss < best_model_info['train_loss'] and attack_loss > best_model_info['attack_loss']:
+        train_loss, attack_loss, best_val_loss, best_ratio, hidden_dim, embedding_dim, encoding_dim, batch_size, lr, best_model, sequence_length, total_epochs = result
+        if best_ratio > best_model_info['atk_val_ratio']:
             best_model_info.update({
                 'train_loss': train_loss,
                 'attack_loss': attack_loss,
+                'val_loss': best_val_loss,
+                'atk_val_ratio': best_ratio,
                 'hidden_dim': hidden_dim,
                 'embedding_dim': embedding_dim,
                 'encoding_dim': encoding_dim,
                 'batch_size': batch_size,
-                'learning_rate': lr
+                'learning_rate': lr,
+                'sequence_length': sequence_length,
+                'total_epochs': total_epochs
             })
+        
+        # Add the current model to the list of top models
+        top_models.append({
+            'train_loss': train_loss,
+            'attack_loss': attack_loss,
+            'val_loss': best_val_loss,
+            'atk_val_ratio': best_ratio,
+            'hidden_dim': hidden_dim,
+            'embedding_dim': embedding_dim,
+            'encoding_dim': encoding_dim,
+            'batch_size': batch_size,
+            'learning_rate': lr,
+            'sequence_length': sequence_length,
+            'total_epochs': total_epochs
+        })
 
-    print("Best Model:", best_model_info)
+    # Sort the top models based on atk_val_ratio in descending order
+    top_models.sort(key=lambda x: x['atk_val_ratio'], reverse=True)
+
+    # Print the top 10 models
+    print("Top 10 Models:")
+    for i, model in enumerate(top_models[:10]):
+        print(f"Rank {i+1}: {model}")
+    
+    # Store top results in file
+    with open('results.txt', 'w') as f:
+        for model in top_models:
+            f.write(f"{model}\n")
+
--- system_calls/grid_search_helper.py
+++ system_calls/grid_search_helper.py
@@ -3,92 +3,157 @@ import numpy as np
 import os
 import torch
 import torch.nn as nn
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import TensorDataset, DataLoader, random_split
 from Autoencoder import Autoencoder
+from os import getpid
+import time
 
 # ? make into class
 
-def train_and_evaluate(train_loader, sequence_length, num_epochs, train_dataset, attack_data_master_path, num_unique_syscalls, hidden_dim, embedding_dim, encoding_dim, batch_size, lr):
+def train_and_evaluate(train_loader, val_loader, sequence_length, num_epochs, train_dataset, attack_data_master_path, num_unique_syscalls, hidden_dim, embedding_dim, encoding_dim, batch_size, lr, patience):
     """
     Trains the autoencoder and evaluates it on the attack data. Returns the best model.
     """
+    start_time = time.time()
+
+    # load_attack_data_start_time = time.time()
+
+    # Cache attack data
+    attack_data_cache = load_all_attack_data(attack_data_master_path, sequence_length, batch_size)
+
+    # print("Process ID: {}, Attack data loading duration: {:.2f}s".format(getpid(), time.time() - load_attack_data_start_time))
+
     # Initialize model
     autoencoder = Autoencoder(sequence_length=sequence_length, num_system_calls=num_unique_syscalls, embedding_dim=embedding_dim, encoding_dim=encoding_dim, hidden_dim=hidden_dim)
-    optimizer = optim.Adam(autoencoder.parameters(), lr=lr)
+    optimizer = optim.SGD(autoencoder.parameters(), lr=lr)
     criterion = nn.MSELoss()
 
     # Print which hyperparameters we're using
     print("Training with hidden_dim: {}, embedding_dim: {}, encoding_dim: {}, batch_size: {}, lr: {}".format(hidden_dim, embedding_dim, encoding_dim, batch_size, lr))
 
-    # DataLoaders
-    # train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
     best_train_loss = float('inf')
+    best_val_loss = float('inf')
     best_attack_loss = float('-inf')
+    best_ratio = float('-inf')
+    total_epochs = 0
+    epochs_without_improvement = 0
+    best_model_state = None
 
+    # Training model
     for epoch in range(num_epochs):
-        # Training
-        # autoencoder.train()
+        total_epochs += 1
+        # epoch_start_time = time.time()
+        # print("-- Process ID: {}, Epoch: {}".format(getpid(), epoch))
+        autoencoder.train()
         
-        # TODO: Early stopping
-        # TODO: Save best model
+        # Early stopping
+        if epoch > patience and epochs_without_improvement > patience:
+            break
 
         train_loss = 0.0
-        for batch in train_loader:
+        for i, batch in enumerate(train_loader):
+            # batch_start_time = time.time()
+
             inputs = batch[0]
+            # embedding_start_time = time.time()
             embedded_inputs = autoencoder.embedding(inputs)
             embedded_inputs = embedded_inputs.view(inputs.size(0), -1)
+            # print(f"Embedding time: {time.time() - embedding_start_time:.4f}s")
 
-            # Forward pass and loss computation
+            # forward_start_time = time.time()
             outputs = autoencoder(inputs)
-            outputs= outputs.view(inputs.size(0), -1)
-            
-            loss = criterion(outputs, embedded_inputs)
+            outputs = outputs.view(inputs.size(0), -1)
+            # print(f"Forward pass time: {time.time() - forward_start_time:.4f}s")
 
+            # loss_start_time = time.time()
+            loss = criterion(outputs, embedded_inputs)
+            # print(f"Loss computation time: {time.time() - loss_start_time:.4f}s")
+
+            # backward_start_time = time.time()
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            # print(f"Backward pass and optimization time: {time.time() - backward_start_time:.4f}s")
 
             train_loss += loss.item()
-        
+
+            # print(f"Total time for batch {i}: {time.time() - batch_start_time:.4f}s\n")
+                
         train_loss /= len(train_loader)
 
-        # Test on attack data
-        attack_loss = attack_test(autoencoder, criterion, attack_data_master_path, sequence_length, batch_size)
+        # Validation step
+        autoencoder.eval()  # Set the model to evaluation mode
+        with torch.no_grad():
+            val_loss = 0.0
+            for batch in val_loader:
+                inputs = batch[0]
+                embedded_inputs = autoencoder.embedding(inputs).view(inputs.size(0), -1)
 
-        if train_loss < best_train_loss and attack_loss > best_attack_loss:
-            best_train_loss = train_loss
-            best_attack_loss = attack_loss
+                outputs = autoencoder(inputs).view(inputs.size(0), -1)
+                loss = criterion(outputs, embedded_inputs)
+                val_loss += loss.item()
 
-        print("Epoch: {}, Train Loss: {}, Attack Loss: {}".format(epoch, train_loss, attack_loss))
+            val_loss /= len(val_loader)
 
-    return best_train_loss, best_attack_loss, hidden_dim, embedding_dim, encoding_dim, batch_size, lr
+            # Test on attack data
+            attack_test_start = time.time()
+            attack_loss = attack_test(autoencoder, criterion, attack_data_cache)
+            # print("Process ID: {}, Attack test duration: {:.2f}s".format(getpid(), time.time() - attack_test_start))
 
+        # Calculate the relative difference (ratio)
+        current_ratio = attack_loss / val_loss if val_loss != 0 else float('inf')
 
-def attack_test(autoencoder, criterion, attack_data_master_path, sequence_length, batch_size):
-    """
-    Tests the autoencoder on the attack data. Returns the average attack loss.
-    """
-    attack_losses = []
+        # Update model saving and early stopping conditions
+        if current_ratio > best_ratio:
+            best_ratio = current_ratio
+            if train_loss < best_train_loss:
+                best_train_loss = train_loss
+            if attack_loss > best_attack_loss:
+                best_attack_loss = attack_loss
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
 
-    # Process each attack data folder
+            epochs_without_improvement = 0
+            best_model_state = autoencoder.state_dict()
+        else:
+            epochs_without_improvement += 1
+
+        # Early stopping check
+        if epochs_without_improvement >= patience:
+            break
+
+        # print("Process ID: {}, Epoch: {}, Train Loss: {}, Attack Loss: {}, Validation Loss: {}, Atk/Val ratio: {}".format(getpid(), epoch, train_loss, attack_loss, val_loss, current_ratio))
+        # print("Process ID: {}, Epoch duration: {:.2f}s".format(getpid(), time.time() - epoch_start_time))
+
+    print("Total training duration: {:.2f}s, Total Epochs: {}".format(time.time() - start_time, total_epochs))
+    return best_train_loss, best_attack_loss, best_val_loss, best_ratio, hidden_dim, embedding_dim, encoding_dim, batch_size, lr, best_model_state, sequence_length, total_epochs
+
+# Function to load all attack data
+def load_all_attack_data(attack_data_master_path, sequence_length, batch_size):
+    attack_data_cache = {}
     for folder_name in os.listdir(attack_data_master_path):
         folder_path = os.path.join(attack_data_master_path, folder_name)
         if os.path.isdir(folder_path):
-            attack_loss = test_on_folder(autoencoder, criterion, folder_path, sequence_length, batch_size )
-            attack_losses.append(attack_loss)
+            attack_data, attack_loader, _, _ = load_data(folder_path, sequence_length, batch_size, val_split=0.0)
+            attack_data_cache[folder_name] = (attack_data, attack_loader)
+    return attack_data_cache
 
-    # Calculate and print average attack loss
-    avg_attack_loss = np.mean(attack_losses)
-
-    return avg_attack_loss
-
-def test_on_folder(autoencoder, criterion, folder_path, sequence_length, batch_size):
+def attack_test(autoencoder, criterion, attack_data_cache):
     """
-    Tests the autoencoder on the attack data in the given folder. Returns the average attack loss.
+    Tests the autoencoder on the attack data. Returns the average attack loss.
+    """
+    attack_loss = 0.0
+
+    for folder_name, (attack_data, attack_loader) in attack_data_cache.items():
+        attack_loss += test_on_folder(autoencoder, criterion, attack_loader)
+
+    return attack_loss/len(attack_data_cache.items())
+
+def test_on_folder(autoencoder, criterion, attack_loader):
+    """
+    Tests the autoencoder on the attack data in the given folder. Returns the average loss.
     """
     # Load attack data from the folder
-    attack_data, attack_loader, num_unique_syscalls = load_data(folder_path, sequence_length, batch_size)
     attack_loss = 0.0
 
     # Calculate loss for each batch in the attack data
@@ -99,11 +164,11 @@ def test_on_folder(autoencoder, criterion, folder_path, sequence_length, batch_s
         loss = criterion(outputs, embedded_inputs)
         attack_loss += loss.item()
 
-    return attack_loss/len(attack_data)
+    return attack_loss/len(attack_loader)
 
-def load_data(folder_path, sequence_length, batch_size):
+def load_data(folder_path, sequence_length, batch_size, val_split=0.3):
     """
-    Loads the data from the given folder path. Returns the dataset and DataLoader.
+    Loads the data from the given folder path. Returns the dataset and DataLoaders.
     """
     data = []
     unique = []
@@ -132,5 +197,12 @@ def load_data(folder_path, sequence_length, batch_size):
     tensors = [torch.tensor(x) for x in mapped_data]
     tensor = torch.stack(tensors)
     dataset = TensorDataset(tensor)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-    return dataset, dataloader, num_unique_syscalls
+
+    val_size = int(len(dataset) * val_split)
+    train_size = len(dataset) - val_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+
+    return dataset, train_loader, val_loader, num_unique_syscalls
