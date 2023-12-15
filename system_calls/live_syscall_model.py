f506f9a580f7859518ed70dc4f4a8c652448967c
johanneslil <jocka.liljedahl@gmail.com>
2023-12-15 16:23:09 +0100

    perf: syscall batch processing massive speedup

--- system_calls/live_syscall_model.py
+++ system_calls/live_syscall_model.py
@@ -36,62 +36,74 @@ def preprocess_sequence(sequence, mapping, sequence_length):
     return torch.tensor(mapped_sequence, dtype=torch.long).unsqueeze(0)  # Use torch.long
 
 
-
-def live_read_and_process(file_path, model, syscall_mapping, sequence_length, threshold):
+def live_read_and_process(file_path, model, syscall_mapping, sequence_length, threshold, read_size, batch_size):
     """
     Continuously read and process system call sequences from the given file using a sliding window.
     """
-    criterion = nn.MSELoss()
+    criterion = nn.MSELoss(reduction='none')
     last_position = 0  # Keep track of the last read position in the file
+
+    # Initialize an empty buffer
+    buffer = []
 
     while True:
         time.sleep(0.5)  # FIXME: adjust or the sleep time later
+        print('Waiting for new data...')
 
-        # TODO: Only read specified number of bytes each time
-        # - Finetune based on performance and time to read+process
-
-        # Time logging
-        start_read_time = time.time()
         with open(file_path, 'r') as file:
             file.seek(last_position)  # Move to the last read position
-            content = file.read()
+            content = file.read(read_size)  # Read 'size' number of bytes
             last_position = file.tell()  # Update the last read position
-
-            end_read_time = time.time()
-            print(f"Read {len(content)} bytes in {end_read_time - start_read_time} seconds") if content else print("Waiting for input data...")
-
-            start_process_time = time.time()
 
             syscalls = content.strip().split()
             num_sequences = len(syscalls) - sequence_length + 1 if len(syscalls) >= sequence_length else 0
-            print(f"Number of sequences: {num_sequences}")
-            # Process the content in a sliding window (sequence) manner
+
+            start_time = time.time()
             for i in range(num_sequences):
                 sequence = syscalls[i:i + sequence_length]
-                formatted_sequence = ', '.join(f"{elem:>3}" for elem in sequence) # printing formatting
                 tensor_sequence = preprocess_sequence(sequence, syscall_mapping, sequence_length)
 
-                # Extract embedded input
-                embedded_inputs = model.embedding(tensor_sequence).view(tensor_sequence.size(0), -1)
+                # Add the tensor sequence to the buffer
+                buffer.append(tensor_sequence)
 
-                # Get the model's output
-                outputs = model(tensor_sequence).view(tensor_sequence.size(0), -1)
+                # If the buffer is full, process a batch and remove it from the buffer
+                if len(buffer) >= batch_size:
+                    # Convert the batch to a tensor
+                    tensor_batch = torch.stack(buffer[:batch_size])
 
-                # Compute loss and classify
-                loss = criterion(outputs, embedded_inputs)
-                classification = 'POSSIBLE INTRUSION' if loss.item() > threshold else 'Normal'
-                print(f"Sequence: [{formatted_sequence}] - Classification: {classification}, Loss: {loss.item():.6f}")
+                    # Extract embedded input
+                    embedded_inputs = model.embedding(tensor_batch).view(tensor_batch.size(0), -1)
 
-            end_process_time = time.time()
-            print(f"Processed {num_sequences} sequences in {end_process_time - start_process_time} seconds")
+                    # Get the model's output
+                    outputs = model(tensor_batch).view(tensor_batch.size(0), -1)
 
+                    # Compute loss and classify
+                    losses = criterion(outputs, embedded_inputs).mean(dim=1)
+                    classifications = ['POSSIBLE INTRUSION' if loss.item() > threshold else 'Normal' for loss in losses]
+
+                    # # Print the classifications and losses
+                    # for sequence, classification, loss in zip(buffer[:batch_size], classifications, losses):
+                    #     formatted_sequence = ', '.join(str(elem) for elem in sequence)
+                    #     print(f"Sequence: [{formatted_sequence}] - Classification: {classification}, Loss: {loss.item():.6f}")
+                    
+
+                    end_time = time.time()
+                    num_sequences = len(buffer[:batch_size])
+                    time_taken = end_time - start_time
+                    sequences_per_second = num_sequences / time_taken
+                    print(f"Processed {num_sequences} sequences in {time_taken:.2f} seconds ({sequences_per_second:.2f} sequences/second)")
+                    start_time = time.time()
+
+                    # Remove the processed batch from the buffer
+                    buffer = buffer[batch_size:]
 
 
 if __name__ == '__main__':
-    # 470 system calls according to https://www.man7.org/linux/man-pages/man2/syscalls.2.html
-    # (round to 500 to be safe)
-    # num_system_calls = 500
-    num_system_calls = 174 # FIXME: change to 500 when model is retrained accordingly
+    # ~470 syscalls according to https://www.man7.org/linux/man-pages/man2/syscalls.2.html
+    # ~310 syscalls according to https://filippo.io/linux-syscall-table/
+    # ~380 syscalls according to unist_64.h file
+
+    num_system_calls = 174 # FIXME: change when model is retrained accordingly
 
     # Load the model info
     with open('trained_models/model_0_info.json', 'r') as f:
@@ -110,4 +122,4 @@ if __name__ == '__main__':
     # TODO: abstract the threshold to a config file and load continuously in the read loop.
     threshold = 1.0 # FIXME: adjust after retraining the model
 
-    live_read_and_process(file_path, model, syscall_mapping, sequence_length, threshold)
+    live_read_and_process(file_path, model, syscall_mapping, sequence_length, threshold, read_size=999999999999, batch_size=10000)
